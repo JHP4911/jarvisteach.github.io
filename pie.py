18bc4fffd9c77dcc52f4da96747229889e84f68a
Richard Jarvis <rjarvis@rwba.org.uk>
2016-06-02 16:32:33 +0100

    latest build

--- pie.py
+++
@@ -1,5 +0,0 @@
-from rwbatools import gui
-
-app=gui()
-app.addPieChart("p1", [50, 200, 75, 300, 150], size=300)
-app.go()
--- simpleApp.py
+++
@@ -1,9 +0,0 @@
-# import the library
-from rwbatools import gui
-
-app = gui()                 # top slice - CREATE the GUI
-
-app.addLabel("title", "Welcome to RWBAtools")   # add a label
-app.setLabelBg("title", "red")                  # set the label's background to be red
-
-app.go()                    # bottom slice - START the GUI
